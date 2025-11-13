8167a4ce8f28e04f700333a3641b8cf05cf4a224
grant <grantschmidt8@gmail.com>
2025-11-13 12:37:16 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/games", (req, res) => res.sendFile(path.join(__dirname, "games.html")));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
